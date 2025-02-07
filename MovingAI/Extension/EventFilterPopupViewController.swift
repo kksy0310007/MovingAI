cbbb4e23454f255fa76bddc87a1395d1dd192008
‘kksy0310007’ <ksy0310007.ys@gmail.com>
2025-02-07 10:52:57 +0900

    eventFilter popup -> picker/ datePicker

--- MovingAI/Extension/EventFilterPopupViewController.swift
+++ MovingAI/Extension/EventFilterPopupViewController.swift
@@ -10,7 +10,6 @@ import SnapKit
 
 protocol EventFilterPopupDelegate: AnyObject {
     func popupDidSelectButton()
-
 }
 
 class EventFilterPopupViewController: UIViewController {
@@ -24,6 +23,15 @@ class EventFilterPopupViewController: UIViewController {
     
     // 델리게이트 선언
     weak var delegate: EventFilterPopupDelegate?
+    
+    let pickerView = UIPickerView()
+    let pickerToolbar = UIToolbar()
+    
+    let datePickerView = UIDatePicker()
+    
+    var selectedPickerType: Int = 0  // 1: 첫 번째 피커, 2: 두 번째 피커
+    let firstPickerData = ["Apple", "Banana", "Cherry", "Date"]
+    let secondPickerData = ["Red", "Green", "Blue", "Yellow"]
     
     private let contentView: UIView = {
         let view = UIView()
@@ -56,7 +64,7 @@ class EventFilterPopupViewController: UIViewController {
         button.contentHorizontalAlignment = .leading
         button.titleEdgeInsets = .init(top: 0, left: 20, bottom: 0, right: 0)
         button.imageEdgeInsets = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 0)
-        button.backgroundColor = .lightGray
+        button.backgroundColor = UIColor(_colorLiteralRed: 226/255, green: 226/255, blue: 226/255, alpha: 1.0)
         button.setTitle("2025-02-05", for: .normal)
         button.setImage(image, for: .normal)
         button.tintColor = .gray
@@ -117,7 +125,7 @@ class EventFilterPopupViewController: UIViewController {
         button.setTitle("취소", for: .normal)
         button.tintColor = .gray
         button.setTitleColor(.black, for: .normal)
-        button.backgroundColor = .lightGray
+        button.backgroundColor = UIColor(_colorLiteralRed: 226/255, green: 226/255, blue: 226/255, alpha: 1.0)
         button.layer.cornerRadius = 10
         button.addTarget(self, action: #selector(closeButtonTapped), for: .touchUpInside)
         return button
@@ -138,7 +146,6 @@ class EventFilterPopupViewController: UIViewController {
         super.viewDidLoad()
         
         setConstraint()
-        
         
     }
     
@@ -215,6 +222,48 @@ class EventFilterPopupViewController: UIViewController {
             make.width.equalTo(90)
             make.trailing.equalToSuperview().offset(-40)
         }
+        
+        pickerView.delegate = self
+        pickerView.dataSource = self
+        pickerView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(pickerView)
+        pickerView.isHidden = true
+        pickerView.backgroundColor = .white
+        
+        // Toolbar 설정
+        pickerToolbar.sizeToFit()
+        let doneButton = UIBarButtonItem(title: "완료", style: .done, target: self, action: #selector(dismissPicker))
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        pickerToolbar.setItems([flexSpace, doneButton], animated: false)
+        pickerToolbar.isHidden = true
+        
+        view.addSubview(pickerToolbar)
+        
+        pickerView.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview()
+            make.bottom.equalToSuperview()
+            make.height.equalTo(200)
+        }
+                
+        pickerToolbar.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview()
+            make.bottom.equalTo(pickerView.snp.top)
+            make.height.equalTo(50)
+        }
+        
+        datePickerView.datePickerMode = .date
+        datePickerView.preferredDatePickerStyle = .inline
+        datePickerView.locale = Locale(identifier: "ko-KR")
+        datePickerView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(datePickerView)
+        datePickerView.isHidden = true
+        datePickerView.backgroundColor = .white
+        datePickerView.addTarget(self, action: #selector(handleDatePicker(_:)), for: .valueChanged)
+        datePickerView.snp.makeConstraints { make in
+            make.centerX.centerY.equalToSuperview()
+            make.height.equalTo(400)
+        }
+        
     }
     
     @objc private func closeButtonTapped() {
@@ -223,17 +272,93 @@ class EventFilterPopupViewController: UIViewController {
     
     @objc private func okButtonTapped() {
         print("okButtonTapped")
+        
+        delegate?.popupDidSelectButton()
     }
     
     @objc private func dateFilterButtonTapped() {
         print("dateFilterButtonTapped")
+        showDatePicker()
     }
     
     @objc private func deviceFilterButtonTapped() {
         print("deviceFilterButtonTapped")
+        selectedPickerType = 1
+        pickerView.reloadAllComponents()
+        showPicker()
     }
     
     @objc private func eventFilterButtonTapped() {
         print("eventFilterButtonTapped")
+        selectedPickerType = 2
+        pickerView.reloadAllComponents()
+        showPicker()
     }
+    
+    @objc private func handleDatePicker(_ sender: UIDatePicker) {
+        dateFilterButton.setTitle(dateFormat(date: sender.date), for: .normal)
+        datePickerView.isHidden = true
+    }
+
+    // PickerView 표시 (애니메이션 효과)
+    func showPicker() {
+        pickerView.isHidden = false
+        pickerToolbar.isHidden = false
+        pickerView.transform = CGAffineTransform(translationX: 0, y: pickerView.frame.height)
+        pickerToolbar.transform = CGAffineTransform(translationX: 0, y: pickerToolbar.frame.height)
+            
+        self.pickerView.transform = .identity
+        self.pickerToolbar.transform = .identity
+    }
+    
+    func showDatePicker() {
+        datePickerView.isHidden = false
+        datePickerView.transform = CGAffineTransform(translationX: 0, y: pickerView.frame.height)
+        self.datePickerView.transform = .identity
+    }
+        // PickerView 숨김
+    @objc func dismissPicker() {
+        self.pickerView.isHidden = true
+        self.pickerToolbar.isHidden = true
+    }
+    
+    private func dateFormat(date: Date) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        
+        return formatter.string(from: date)
+    }
+}
+extension EventFilterPopupViewController: UIPickerViewDelegate, UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+
+        if selectedPickerType == 1 {
+            return firstPickerData[row]
+        } else {
+            return secondPickerData[row]
+        }
+    }
+
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if selectedPickerType == 1 {
+            return firstPickerData.count
+        } else {
+            return secondPickerData.count
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let selectedValue = selectedPickerType == 1 ? firstPickerData[row] : secondPickerData[row]
+        
+        if selectedPickerType == 1 {
+            deviceFilterButton.setTitle(selectedValue, for: .normal)
+        } else {
+            eventFilterButton.setTitle(selectedValue, for: .normal)
+        }
+    }
+    
 }
